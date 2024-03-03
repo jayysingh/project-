0a0b4bc316cdf51f6702962fedc14ce92af4cffe
jayysingh <64648208+jayysingh@users.noreply.github.com>
2024-03-03 23:16:15 +0530

    add 1
    sharpner module dom manupulation

---
+++ byid.js
@@ -0,0 +1,13 @@
+const x =document.getElementById('header');
+x.style.backgroundColor = 'green';
+x.style.borderBottom = '3px solid orange';
+
+const y = document.getElementById('main-heading');
+y.textContent = 'Fruit World';
+y.style.color = "orange";
+
+const z =document.getElementById('basket-heading');
+z.style.color = 'green';
+
+const greet = document.getElementById('thanks');
+greet.innerHTML = '<p>Please visit us again</p>';
